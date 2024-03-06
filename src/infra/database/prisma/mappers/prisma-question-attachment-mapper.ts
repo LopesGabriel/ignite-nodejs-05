99fa155ffaa22a8ee2ff1ead0ac2a059fc9bb617
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-03-06 08:59:07 -0300

    feat: add models to Prisma schema and implement prisma mappers

---
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -0,0 +1,19 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export class PrismaQuestionAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): QuestionAttachment {
+    if (!raw.questionId) {
+      throw new Error('Invalid attachment type')
+    }
+
+    return QuestionAttachment.create(
+      {
+        questionId: new UniqueEntityId(raw.questionId),
+        attachmentId: new UniqueEntityId(raw.id),
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+}
